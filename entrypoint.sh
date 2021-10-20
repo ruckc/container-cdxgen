9a63e3d4cb16db0585327483a99c99d21f87506b
Curtis Ruck <curtis@ruck.io>
2021-10-20 16:47:01 -0400

    actually upload the bom

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ set -x
 echo "===== GIT CLONE ====="
 git clone $GIT_URL repo
 echo "===== CDXGEN    ====="
-cdxgen -o bom.xml repo
+cdxgen -o bom.json repo
 if [ "${BOM_UPLOAD_URL}" != "" ]; then
   echo "===== CURL UP   ===== $BOM_UPLOAD_URL"
-  curl -v -XPOST "$BOM_UPLOAD_URL"
+  curl -v -XPOST "$BOM_UPLOAD_URL" -d @bom.json
 fi
