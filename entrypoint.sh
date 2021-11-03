6885cdc2676ec1b328673afff1a0d1b1957ee9b9
Curtis Ruck <curtis@ruck.io>
2021-11-03 09:29:34 -0400

    specify upload content type

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ for REF in $(echo ${GIT_REFS} | sed "s/,/ /g"); do
   if [ "${BOM_BASE_UPLOAD_URL}" != "" ]; then
     URL="${BOM_BASE_UPLOAD_URL}/${REF}"
     echo "===== CURL UP   ===== $URL"
-    curl -v -XPOST "$URL" -d @bom.json
+    curl -v -XPOST "$URL" -H "Content-Type: application/json" -d @bom.json
   fi
   rm bom.json
 done
