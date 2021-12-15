ad201d962ca71f2ac32e01c8bfcf621fc602c2ca
Curtis Ruck <curtis@ruck.io>
2021-12-15 16:36:39 -0500

    add call to upload error from cdxgen if bom.json is not generated

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,14 @@ git clone $GIT_URL repo
 cd repo
 for REF in $(echo ${GIT_REFS} | sed "s/,/ /g"); do
   git checkout $REF
-  cdxgen -o bom.json .
+  OTUPUT=$(cdxgen -o bom.json . 2>&1)
   if [ "${BOM_BASE_UPLOAD_URL}" != "" ]; then
     URL="${BOM_BASE_UPLOAD_URL}/${REF}"
     echo "===== CURL UP   ===== $URL"
     curl -v -XPOST "$URL" -H "Content-Type: application/json" -d @bom.json
+  else
+    URL="${BOM_BASE_UPLOAD_URL}/${REF}/error"
+    echo "${ERRORS}" | curl -v -XPOST "$URL" -H "Content-Type: text/plain" -d -
   fi
   rm bom.json
 done
