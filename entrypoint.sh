898f2e1ee112a905cdf4be60d1121a55cb9d40ce
Curtis Ruck <curtis@ruck.io>
2021-12-16 09:56:48 -0500

    further fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,16 @@ GIT_REFS=$(curl --fail -v -XGET "${BOM_URL}" | jq -r '.hash[]')
 
 for REF in $(echo ${GIT_REFS} | sed "s/,/ /g"); do
   git -c advice.detachedHead=false checkout $REF
-  ERRORS=$(cdxgen -r -o bom.json . 2>&1)
+  cdxgen -r -o bom.json . 2>&1 | tee output.txt
   ls -latr
   if [ ! -f "bom.json" ]; then
     echo "Unable to find generated bom.json"
     URL="${BOM_BASE_UPLOAD_URL}/${REF}/error"
-    #SUCCESS=0
-    #while [ $SUCCESS -ne 1 ]; do
+    SUCCESS=0
+    while [ $SUCCESS -ne 1 ]; do
       echo "===== CURL ERR UP ===== $URL"
-      echo "${ERRORS}" | curl --fail -v -XPOST "$URL" -H "Content-Type: text/plain" -T -
-    #done
+      echo "${ERRORS}" | curl --fail -v -XPOST "$URL" -H "Content-Type: text/plain" -d @output.txt && SUCCESS=1 || sleep 5
+    done
     continue
   fi
   if [ "${BOM_BASE_UPLOAD_URL}" != "" ]; then
@@ -31,5 +31,5 @@ for REF in $(echo ${GIT_REFS} | sed "s/,/ /g"); do
       curl --fail -v -XPOST "$URL" -H "Content-Type: application/json" -d @bom.json && SUCCESS=1 || sleep 5
     done
   fi
-  rm -rv bom.json bom.xml
+  rm -rv bom.json bom.xml output.txt
 done
