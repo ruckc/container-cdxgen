62fe303a9faf5de2f4fb773777e65c220542ad3d
Curtis Ruck <curtis@teamomni.com>
2022-01-28 12:28:14 -0500

    update entrypoint to properly urlEncode commitTimestamp

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,11 @@ for REF in $(echo ${GIT_REFS} | sed "s/,/ /g"); do
   ls -latr
   if [ ! -f "bom.json" ]; then
     echo "Unable to find generated bom.json"
-    URL="${BOM_BASE_UPLOAD_URL}/${REF}/error?commitTimestamp=${COMMIT_DATE}"
+    URL="${BOM_BASE_UPLOAD_URL}/${REF}/error"
     SUCCESS=0
     while [ $SUCCESS -ne 1 ]; do
       echo "===== CURL ERR UP ===== $URL"
-      echo "${ERRORS}" | curl --fail -v -XPOST "$URL" -H "Content-Type: text/plain" -d @output.txt && SUCCESS=1 || sleep 5
+      echo "${ERRORS}" | curl --fail -v -XPOST "$URL" -G --data-urlencode "commitTimestamp=${COMMIT_DATE}" -H "Content-Type: text/plain" -d @output.txt && SUCCESS=1 || sleep 5
     done
     continue
   fi
@@ -30,7 +30,7 @@ for REF in $(echo ${GIT_REFS} | sed "s/,/ /g"); do
     SUCCESS=0
     while [ $SUCCESS -ne 1 ]; do
       echo "===== CURL UP     ===== $URL"
-      curl --fail -v -XPOST "$URL" -H "Content-Type: application/json" -d @bom.json && SUCCESS=1 || sleep 5
+      curl --fail -v -XPOST "$URL" -G --data-urlencode "commitTimestamp=${COMMIT_DATE}" -H "Content-Type: application/json" -d @bom.json && SUCCESS=1 || sleep 5
     done
   fi
   rm -rv bom.json bom.xml output.txt
